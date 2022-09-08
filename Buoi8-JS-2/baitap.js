4c5743a8afd4490aa11f66ea1f6fd786301a8d55
Hung Nguyen <hung.140502@gmail.com>
2022-09-08 21:31:53 +0700

    buoi 8 second half

--- Buoi8-JS-2/baitap.js
+++ Buoi8-JS-2/baitap.js
@@ -35,4 +35,18 @@ let number = '10.5';
 // console.log(Number.parseInt(number));
 // random số;
 //trả về 1 số ngẫu nhiên trong khoảng từ 0-1
-console.log(Math.ceil(6 * Math.random()));
+// console.log(Math.ceil(6 * Math.random()));
+// Math.floor(Math.random() * (max - min + 1) + min);
+
+//Xử lý string
+let dataT= " hung nguyen "; // 'hung'
+let dataT1 = `alo ${dataT}`;
+console.log('den so ki tu ', dataT.length);
+console.log('bo khoang trang 2 ben', dataT.trim());
+console.log('viet hoa tat ca cac ki tu: ', dataT.toUpperCase());
+console.log('viet thuong tat ca cac ki tu: ', dataT.toLowerCase());
+console.log('Ky tu thu n : ', dataT.charAt(4));
+console.log('Kiem tra ki tu nhap vao co trong chuoi khong ', dataT.includes('hung'));
+console.log('Cat chuoi tu vi tri [start;end) ', dataT.slice(0,5));
+//indexOf vi tri cua cuoi a trong chuoi b: a.indexOf(b);
+//BTVN B1--> B10 trong word (2,4,6,7,8,9,10)
